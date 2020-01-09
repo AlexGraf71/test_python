dceabc92f29a21057d2fe3283fffdb20227553fd
AlexGraf <lexraf@list.ru>
2020-01-09 22:37:00 +0300

    выделен класс для управления сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,9 +12,9 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="123", middlename="123", lastname="123",
                                nickname="123", company="123", address="123",
                                home="123", mobile="123", work="123", fax="123",
                                email="123"))
-    app.logout()
+    app.session.logout()
