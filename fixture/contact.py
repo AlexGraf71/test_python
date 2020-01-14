0e1f0227b5389aeb6f1954b2a384fb93439f7da5
a.zakharov <lexraf@list.ru>
2020-01-14 14:50:47 +0300

    Кеширование и хеширование

--- fixture/contact.py
+++ fixture/contact.py
@@ -14,7 +14,6 @@ class ContactHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def fill_contact_form(self, contact):
-        wd = self.app.wd
         self.change_field_value("firstname", contact.firstname)
         self.change_field_value("middlename", contact.middlename)
         self.change_field_value("lastname", contact.lastname)
@@ -33,6 +32,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_name("submit").click()
         self.app.return_home()
+        self.contact_cache = None
 
     def delete_first_contact(self):
         wd = self.app.wd
@@ -40,6 +40,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("/html/body/div[1]/div[4]/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def modify_first_contact(self, contact):
         wd = self.app.wd
@@ -48,23 +49,27 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_name("update").click()
         self.app.return_home()
+        self.contact_cache = None
 
     def count(self):
         wd = self.app.wd
         self.app.return_home()
-        return len(wd.find_element_by_name("selected[]"))
+        return len(wd.find_elements_by_name("selected[]"))
 
     def open_add_contact_page(self):
         wd = self.app.wd
         if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("photo")) > 0):
             wd.find_element_by_link_text("add new").click()
 
+    contact_cache = None
+
     def get_contacts_list(self):
-        wd = self.app.wd
-        contacts = []
-        for element in wd.find_elements_by_xpath("//table[@id='maintable']/tbody/tr[@name='entry']"):
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            lastname = element.find_element_by_xpath(".//td[2]").text
-            firstname = element.find_element_by_xpath(".//td[3]").text
-            contacts.append(Contact(lastname=lastname, firstname=firstname, id=id))
-        return contacts
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.contact_cache = []
+            for element in wd.find_elements_by_xpath("//table[@id='maintable']/tbody/tr[@name='entry']"):
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                lastname = element.find_element_by_xpath(".//td[2]").text
+                firstname = element.find_element_by_xpath(".//td[3]").text
+                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id))
+        return list(self.contact_cache)
